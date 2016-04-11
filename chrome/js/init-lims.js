4a256fa33358f6445a4e8bc1024cbd1587acaa93
Adrian Nicoara <adrian.nicoara@bgasoft.com>
2016-04-11 15:37:23 +0300

    lims chrome app and test client update. TCP connection works. Communication between website and chrome app works in both ways

--- chrome/js/init-lims.js
+++ chrome/js/init-lims.js
@@ -1,3 +1,11 @@
+function stringToBinary(str)
+{
+    var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+    var bufView = new Uint16Array(buf);
+    for (var i=0, strLen=str.length; i<strLen; i++)
+        bufView[i] = str.charCodeAt(i);
+    return buf;
+}
 function send_udp_message(host,port,message) {
     var current_socket_id=0;
     chrome.sockets.udp.create({}, function(createInfo) {
@@ -8,46 +16,84 @@ function send_udp_message(host,port,message) {
         });
     });
 }
-function send_tcp_message(host,port,message) {
+function send_tcp_message(host,port,message,communication_port) {
     var current_socket_id=0;
     chrome.sockets.tcp.create({}, function(createInfo) {
         current_socket_id=createInfo.socketId;
         chrome.sockets.tcp.connect(current_socket_id,
-        host, port, function(){
-            chrome.sockets.tcp.send(current_socket_id, message, function(sendInfo){
-                console.log("sent " + sendInfo.bytesSent);
-                chrome.sockets.tcp.disconnect(current_socket_id);
-            });
+        host, port, function(result){
+            if (result === 0) {
+                chrome.sockets.tcp.send(current_socket_id, stringToBinary(message), function(sendInfo){
+                    console.log(sendInfo);
+                    if(sendInfo.bytesSent!=undefined && sendInfo.bytesSent>0)
+                    {
+                        console.log("sent " + sendInfo.bytesSent);
+                        chrome.sockets.tcp.disconnect(current_socket_id);
+                        if(communication_port!=undefined)
+                            communication_port.postMessage({status: true});
+                    } else
+                    {
+                        if(communication_port!=undefined)
+                            communication_port.postMessage({status: false});
+                    }
+                });
+            } else
+                if(communication_port!=undefined)
+                    communication_port.postMessage({status: false});
+
         });
     });
+    console.log('response sent');
 }
-function tcp_print_message(host,port,message)
+function tcp_print_message(host,port,message,communication_port)
 {
-    send_tcp_message(host,port,message);
+    send_tcp_message(host,port,message,communication_port);
 }
 function listen_for_messages()
 {
     chrome.runtime.onMessageExternal.addListener(
-      function(message, sender, sendResponse) {
-        console.log(sender.tab ?
-                    "from a content script:" + sender.tab.url :
-                    "from the extension");
-        console.log(message);
-        if(typeof message.type!="undefined")
-        {
-            switch(message.type)
+        function(message, sender, sendResponse) {
+            console.log(sender);
+            console.log(sender.tab ?"from a content script:" + sender.tab.url :"from the extension");
+            console.log(message);
+            if(typeof message.type!="undefined")
             {
-                case "tcp_print":
-                    if(typeof message.print_host!="undefined" && typeof message.print_port!="undefined" && typeof message.print_message!="undefined")
-                    {
-                        tcp_print_message(message.print_host,message.print_port,message.print_message);
-                    }
-                    break;
+                switch(message.type)
+                {
+                    case "tcp_print":
+                        if(typeof message.print_host!="undefined" && typeof message.print_port!="undefined" && typeof message.print_message!="undefined")
+                            tcp_print_message(message.print_host,parseInt(message.print_port),message.print_message);
+                        break;
+                }
             }
         }
-      });
-      console.log('listening started!');
+    );
+    chrome.runtime.onConnectExternal.addListener(function(communication_port) {
+        communication_port.onMessage.addListener(function(message) {
+            //console.log(sender);
+            //console.log(sender.tab ?"from a content script:" + sender.tab.url :"from the extension");
+            console.log(message);
+            if(typeof message.type!="undefined")
+            {
+                switch(message.type)
+                {
+                    case "tcp_print":
+                        if(typeof message.print_host!="undefined" && typeof message.print_port!="undefined" && typeof message.print_message!="undefined")
+                            tcp_print_message(message.print_host,parseInt(message.print_port),message.print_message,communication_port);
+                        break;
+                }
+            }
+        });
+    });
 }
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('background.html', {
+    'outerBounds': {
+      'width': 400,
+      'height': 500
+    }
+  });
+});
 (function() {
     listen_for_messages();
 })();
--- test/js/test-lims.js
+++ test/js/test-lims.js
@@ -1,31 +1,36 @@
 // The ID of the extension we want to talk to.
-var lims_extension = "cmmjmcappjhjgpjgpaglpfhjijmhnhhb";
+var lims_chromeapp = "cmmjmcappjhjgpjgpaglpfhjijmhnhhb";
 
 function isFirefox()
 {
     return false;
 }
-function limsExtensionTCPPrint(host,port,message,callback)
+function limsChromeAppTCPPrint(host,port,message,callback)
 {
-    sendMessageToLimsExtension({
+    sendMessageToLimsChromeApp({
         type:"tcp_print",
         print_host:host,
         print_port:port,
         print_message:message,
     },callback);
 }
-function sendMessageToLimsExtension(message_content,callback)
+function sendMessageToLimsChromeApp(message_content,callback)
 {
     if(typeof chrome.runtime.sendMessage!="undefined")
     {
-        chrome.runtime.sendMessage(lims_extension, message_content,callback);
+        var port = chrome.runtime.connect(lims_chromeapp);
+        port.postMessage(message_content);
+        port.onMessage.addListener(function(request, sender, sendResponse){
+            callback(request);
+        })
+        //chrome.runtime.sendMessage(lims_chromeapp, message_content,callback);
         console.log("sent");
     } else if(isFirefox())
     {
 
     } else
     {
-        alert('communication with limsAbc extension is not possible');
+        alert('communication with limsAbc chrome app is not possible');
     }
 }
 // Make a simple request:
